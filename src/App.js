33ddabac7e0b8d3560904bec749bb80428f8548a
ghooman <ghoopark1@gmail.com>
2022-07-20 03:03:00 +0900

    navigate, nested routes, outlet

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
 import './App.css';
+import bg from "./img/bg.png"
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import bg from './img/bg.png';
 import { useState } from "react"
 import data from "./data.js";
-import Card from "./Card.js"
+import Card from "./component/Card.js"
+import Detail from './pages/Detail';
+import About from './pages/About';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+
+
 
 function App() {
 
   let [shoes] = useState(data)
+  let navigate = useNavigate();
 
 
   return (
     <div className="App">
+
+
+
+
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">Buble</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg" style={{backgroundImage: 'url('+ bg +')'}}></div>
-      
-      <div className="container">
-        <div className="row">
-          <Card shoes={shoes[0]} i={1}/>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div className="main-bg" style={{backgroundImage: 'url('+ bg +')'}}></div>
+            <div className="container">
+              <div className="row">
+                {
+                  shoes.map((el, i) => {
+                    return (
+                      <Card shoes={shoes[i]} i={i + 1}/>
+                    )
+                  })
+                }
+              </div>
+            </div> 
+          </>
+        } />
+        <Route path='/detail' element={<Detail/>} />
+        <Route path='/about' element={<About/>}>
+          <Route path='member' element={<div>멤버임</div>} />
+          <Route path='location' element={<div>위치임</div>} />
+        </Route>
 
-          {
-            shoes.map((el, i) => {
-              return (
-                <Card shoes={shoes[i]} i={i + 1}/>
-              )
-            })
-          }
-        </div>
-      </div> 
+        <Route path='*' element={<div>없는페이지에요</div>} />
+      </Routes>
+
+
 
 
     </div>
