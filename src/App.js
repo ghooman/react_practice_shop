ebb67796816f9ca48d96f5aeedf883d37da3bbcf
ghooman <ghoopark1@gmail.com>
2022-07-21 02:25:58 +0900

    ajax요청2

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import './App.css';
 import bg from "./img/bg.png"
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from "react"
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 import data from "./data.js";
 import Card from "./component/Card.js"
 import Detail from './pages/Detail';
-import About from './pages/About';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+// import About from './pages/About';
+
 
 
 
 function App() {
 
-  let [shoes] = useState(data)
+  let [shoes, setShoes] = useState(data)
   let navigate = useNavigate();
+  
 
 
   return (
     <div className="App">
-
-
-
-
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">Buble</Navbar.Brand>
@@ -46,14 +46,25 @@ function App() {
                   })
                 }
               </div>
-            </div> 
+            </div>
+            <button onClick={() => {
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((result) => {
+                // let copy = [...shoes].concat(result.data)
+                let copy = [...shoes, ...result.data]
+                setShoes(copy);
+              })
+              .catch(() => {console.log('실패함')})
+            }}>더보기</button>
           </>
         } />
-        <Route path='/detail' element={<Detail/>} />
-        <Route path='/about' element={<About/>}>
+        <Route path='/detail/:id' element={<Detail shoes={shoes}/>} />
+
+
+        {/* <Route path='/about' element={<About/>}>
           <Route path='member' element={<div>멤버임</div>} />
           <Route path='location' element={<div>위치임</div>} />
-        </Route>
+        </Route> */}
 
         <Route path='*' element={<div>없는페이지에요</div>} />
       </Routes>
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,14 +1,43 @@
-const Detail = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+const Detail = (props) => {
+
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find((el) => el.id == id);
+  // let [alert, setAlert] = useState(true); 
+  // let [count, setCount] = useState(0);
+  // let [num, setNum] = useState('');
+
+  // useEffect(() => {
+  //   if (isNaN(num)) {
+  //     alert("숫자만 적으셈")
+  //   }
+  // }, [num])
+
+
+
   return (
+
+
     <div className="container">
+      {/* <input onChange={(e) => {setNum(e.target.value)}}></input>
+      {
+        alert == true
+        ? <div className="alert alert-warning">2초 이내 구매시 할인</div>
+        : null
+      }
+      {count}
+      <button onClick={() => {setCount(count + 1)}}>버튼</button> */}
       <div className="row">
         <div className="col-md-6">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="shoesImg" width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
